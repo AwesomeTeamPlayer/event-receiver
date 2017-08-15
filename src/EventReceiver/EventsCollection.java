965c4c042d71c10250862231424cc35c7661e225
SimonIsMe <szym1991@gmail.com>
2017-08-15 17:25:25 +0200

    Add EventCollection

---
+++ src/EventReceiver/EventsCollection.java
@@ -0,0 +1,25 @@
+package EventReceiver;
+
+import EventReceiver.EventParsers.EventParserInterface;
+import EventReceiver.ValueObjects.Event;
+
+import java.util.LinkedList;
+
+public class EventsCollection
+{
+    private LinkedList<EventParserInterface> eventParsersList = new LinkedList();
+
+    public void add(EventParserInterface event)
+    {
+        this.eventParsersList.add(event);
+    }
+
+    public void tryParse(Event event)
+    {
+        for (EventParserInterface eventParser : this.eventParsersList) {
+            if (eventParser.getEventName().equals(event.getName())) {
+                eventParser.execute(event);
+            }
+        }
+    }
+}
