6e52beca05cd325486caa8699728a24a706a09a7
SimonIsMe <szym1991@gmail.com>
2017-08-15 17:24:16 +0200

    Add JSON jat library

--- compile.sh
+++ compile.sh
@@ -6,6 +6,6 @@ rm -rf /app/out/*
 cd /app/src
 find -name "*.java" > /app/var/sources.txt
 
-javac -d /app/out -cp /app/jar/amqp-client-4.0.2.jar:/app/jar/slf4j-api-1.7.25.jar:/app/jar/slf4j-simple-1.7.25.jar @/app/var/sources.txt
+javac -d /app/out -cp /app/jar/amqp-client-4.0.2.jar:/app/jar/json-20170516.jar:/app/jar/slf4j-api-1.7.25.jar:/app/jar/slf4j-simple-1.7.25.jar @/app/var/sources.txt
 
 
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp ./out:./jar/amqp-client-4.0.2.jar:./jar/slf4j-api-1.7.25.jar:./jar/slf4j-simple-1.7.25.jar EventReceiver.Main
+java -cp ./out:./jar/json-20170516.jar:./jar/amqp-client-4.0.2.jar:./jar/slf4j-api-1.7.25.jar:./jar/slf4j-simple-1.7.25.jar EventReceiver.Main
