e7b84ca7a8500745678f098941457f717ca35c93
SimonIsMe <szym1991@gmail.com>
2017-08-17 21:38:25 +0200

    Use mocks in EventsCollectionTest

--- tests/EventReceiver/EventsCollectionTest.java
+++ tests/EventReceiver/EventsCollectionTest.java
@@ -1,5 +1,6 @@
 package EventReceiver;
 
+import static org.mockito.Mockito.*;
 import EventReceiver.EventParsers.EventParserInterface;
 import EventReceiver.ValueObjects.Event;
 import org.json.JSONObject;
@@ -7,6 +8,7 @@ import org.junit.Test;
 import java.util.Date;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class EventsCollectionTest
 {
@@ -18,6 +20,48 @@ public class EventsCollectionTest
         );
 
         assertFalse(result);
+    }
+
+    @Test
+    public void test_tryParse_when_collection_is_not_empty_but_there_is_not_matched_event_parser() {
+        EventParserInterface eventParser_0 = mock ( EventParserInterface. class );
+        stub(eventParser_0.getEventName()).toReturn("xyz");
+
+        EventParserInterface eventParser_1 = mock ( EventParserInterface. class );
+        stub(eventParser_1.getEventName()).toReturn("abc");
+
+        EventsCollection eventsCollection = new EventsCollection();
+        eventsCollection.add(eventParser_0);
+        eventsCollection.add(eventParser_1);
+
+        boolean result = eventsCollection.tryParse(
+            new Event("name", new Date(), new JSONObject())
+        );
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void test_tryParse_when_collection_is_not_empty_and_there_is_matched_event_parser() {
+        EventParserInterface eventParser_0 = mock ( EventParserInterface. class );
+        stub(eventParser_0.getEventName()).toReturn("xyz");
+
+        EventParserInterface eventParser_1 = mock ( EventParserInterface. class );
+        stub(eventParser_1.getEventName()).toReturn("name");
+
+        EventParserInterface eventParser_2 = mock ( EventParserInterface. class );
+        stub(eventParser_2.getEventName()).toReturn("abc");
+
+        EventsCollection eventsCollection = new EventsCollection();
+        eventsCollection.add(eventParser_0);
+        eventsCollection.add(eventParser_1);
+        eventsCollection.add(eventParser_2);
+
+        boolean result = eventsCollection.tryParse(
+            new Event("name", new Date(), new JSONObject())
+        );
+
+        assertTrue(result);
     }
 
 }
