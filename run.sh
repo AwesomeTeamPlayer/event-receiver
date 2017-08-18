04958b5db085ba7fcb0e71008ecc48cc9ba43045
SimonIsMe <szym1991@gmail.com>
2017-08-18 06:21:03 +0200

    Fix running bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp ./out:./jar/json-20170516.jar:./jar/amqp-client-4.0.2.jar:./jar/slf4j-api-1.7.25.jar:./jar/slf4j-simple-1.7.25.jar EventReceiver.Main
+java -cp ./build:./lib/amqp-client-4.0.2.jar:./lib/json-20170516.jar:./lib/slf4j-api-1.7.25.jar:./lib/slf4j-simple-1.7.25.jar EventReceiver.Main
